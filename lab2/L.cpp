067ed24c47ada6f9933304ea569829f20d11d45c
Temirlan <tzhanshin@mail.ru>
2024-07-01 12:51:31 +0500

    upd

---
+++ lab2/L.cpp
@@ -0,0 +1,102 @@
+#include<iostream>             
+using  namespace std;
+
+template<typename T>
+class List {
+public:
+    List() : Size(0), head(nullptr) {}
+
+    ~List() { 
+        clear(); 
+    }
+
+    int GetSize() const {
+        return Size;
+    }
+
+    void push_back(T data) {
+        if (head == nullptr) {
+            head = new Node(data);
+        } else {
+            Node *current = this->head;
+            while (current->pNext != nullptr) {
+                current = current->pNext;
+            }
+            current->pNext = new Node(data);
+        }
+
+        Size++;
+    }
+
+    int findMaxSumOfSubarray() {
+        if (head == nullptr) return 0;
+
+        int max_current = head->data;
+        int max_global = head->data;
+        Node *current = head->pNext;
+
+        while (current != nullptr) {
+            max_current = max(current->data, max_current + current->data);
+            if (max_global < max_current) {
+                max_global = max_current;
+            }
+            current = current->pNext;
+        }
+
+        return max_global;
+    }
+
+    void clear() {
+        while (Size) {
+            Node *temp = head;
+            head = head->pNext;
+            delete temp;
+
+            Size--;
+        }
+    }
+    
+    T &operator[](const int index) {   
+        int counter = 0;
+        Node *current = this->head;
+        while (current != nullptr) {
+            if (counter == index) {
+                return current->data;
+            }
+            current = current->pNext;
+            counter++;
+        }
+    }
+
+private:
+    class Node {
+    public:
+        Node *pNext;
+        T data;
+
+        Node(T data = T(), Node *pNext = nullptr) {
+            this->data = data;
+            this->pNext = pNext;
+        }
+    };
+
+    int Size;
+    Node *head;
+};
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    List<int> lst;
+    for (int i = 0; i < n; i++) {
+        int data;
+        cin >> data;
+        lst.push_back(data);
+    }
+
+    cout << lst.findMaxSumOfSubarray();
+
+    return 0;
+}
