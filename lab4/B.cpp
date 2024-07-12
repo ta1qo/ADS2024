9af705d1ec3e2bf49a02d00cc7024cce1e6cdede
Temirlan <tzhanshin@mail.ru>
2024-07-12 21:37:08 +0500

    Get subtree

--- lab4/B.cpp
+++ lab4/B.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-// binary search tree implementation
+
 template<typename T>
 class BST {
 public: 
@@ -21,25 +21,6 @@ public:
         return search(root, data) != nullptr;
     }
 
-    T get_min() const {
-        if (root == nullptr) return 0;
-        Node* minNode = get_min(root);
-        return minNode->data;
-    }
-
-    T get_max() const {
-        if (root == nullptr) return 0;
-        Node* maxNode = get_max(root);
-        return maxNode->data;
-    }
-
-    void delete_node(T data) {
-        root = delete_node(root, data);
-    }
-
-    void print_tree() const {
-        print_tree(root); cout << endl;
-    }
     int get_size_of_subtree(int val) const {
         Node* current = search(root, val);
         if (current == nullptr) return 0;
@@ -78,50 +59,9 @@ private:
         return (data < current->data) ? search(current->left, data) : search(current->right, data);
     }
 
-    Node* get_min(Node* current) const {
-        if (current == nullptr) return nullptr;
-        if (current->left == nullptr) return current;
-        return get_min(current->left);
-    }
-
-    Node* get_max(Node* current) const {
-        if (current == nullptr) return nullptr;
-        if (current->right == nullptr) return current;
-        return get_max(current->right);
-    }
-
-    Node* delete_node(Node* current, T data) {
-        if (current == nullptr) return nullptr;
-        else if (data < current->data) current->left = remove(current->left, data);
-        else if (data > current->data) current->right = remove(current->right, data);
-        else {
-            if (current->left == nullptr) {
-                Node* temp = current->right;
-                delete current; size--;
-                return temp;
-            } else if (current->right == nullptr) {
-                Node* temp = current->left;
-                delete current; size--;
-                return temp;
-            } else {
-                Node* maxInLeft = get_max(current->left);
-                current->data = maxInLeft->data;
-                current->left = remove(current->left, maxInLeft->data);
-            }
-        }
-        return current;
-    }
-
     int get_size_of_subtree(Node* current) const {
         if (current == nullptr) return 0;
         return 1 + get_size_of_subtree(current->left) + get_size_of_subtree(current->right);
-    }
-
-    void print_tree(Node* current) {
-        if (current == nullptr) return;
-        print_tree(current->left);
-        cout << current->data << " ";
-        print_tree(current->right);
     }
 
     void clear(Node* current) {
