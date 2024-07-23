14409f664c57f91f0f598cca96849f77dec6952e
Temirlan <tzhanshin@mail.ru>
2024-07-24 01:16:31 +0500

     

---
+++ lab5/C.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct PriorityQueue {
+    vector<int> heap;
+
+    int parent(int i) { return (i - 1) / 2; }
+    int left(int i) { return (2*i + 1); }
+    int right(int i) { return (2*i + 2); }
+    
+    void heapifyUp(int i) {
+        int p = parent(i);
+        if (i != 0 && heap[i] > heap[p]) {
+            swap(heap[i], heap[p]);
+            heapifyUp(p);
+        }
+    }
+
+    void heapifyDown(int i) {
+        int l = left(i);
+        int r = right(i);
+        int max = i;
+        int size = heap.size();
+        if (l < size && heap[max] < heap[l]) max = l;
+        if (r < size && heap[max] < heap[r]) max = r;
+        if (max != i) {
+            swap(heap[i], heap[max]);
+            heapifyDown(max);
+        }
+    }
+
+    int GetSize() {
+        return heap.size();
+    }
+
+    bool isEmpty() {
+        return heap.empty();
+    }
+
+    void push(int x) {
+        heap.push_back(x);
+        heapifyUp(heap.size() - 1);
+    }
+
+    void pop() {
+        if (isEmpty()) return;
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapifyDown(0);
+    }
+
+    int top() {
+        if (!isEmpty()) return heap.front();
+    }
+};
+
+int main() {
+    int n, k; cin >> n >> k;
+    PriorityQueue pq;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        pq.push(x);
+    }
+    int sum = 0;
+    while (k--) {
+        sum += pq.top();
+        int repl = pq.top() - 1;
+        pq.pop();
+        pq.push(repl);
+    }
+    cout << sum;
+    return 0;
+}
