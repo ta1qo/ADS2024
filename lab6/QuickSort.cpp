b47c3c27d61394e3174ecd4993277fbcc760bfbe
Temirlan <tzhanshin@mail.ru>
2024-07-24 19:18:40 +0500

    qsort

---
+++ lab6/QuickSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int left, int right) {
+    int pivot = arr[right];
+    int i = (left-1);
+    for (int j = left; j <= right-1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[right]);
+    return (i+1);
+}
+
+void quickSort(int arr[], int left, int right) {
+    if (left >= right) return;
+    int index = partition(arr, left, right);
+    quickSort(arr, left, index-1);
+    quickSort(arr, index+1, right);
+}
+
+int main() {
+    int n; cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    quickSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
