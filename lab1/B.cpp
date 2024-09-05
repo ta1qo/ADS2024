5fba2075717a9491d29e5ccbb5e0fb1eef3cc0b5
Temirlan <tzhanshin@mail.ru>
2024-09-05 16:23:12 +0500

    edited

--- lab1/B.cpp
+++ lab1/B.cpp
@@ -1,27 +1,20 @@
 #include<iostream>
-#include<queue>
+#include<stack>
 using namespace std;
 
 int main() {
-    int n; 
-    cin >> n;
-    queue<int> q;
+    int n; cin >> n;
+    int arr[n];
+    stack<int> s;
     for (int i = 0; i < n; i++) {
-        int x; 
-        cin >> x;
-        q.push(x);
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++) {
+        while (!s.empty() && s.top() > arr[i]) s.pop();
+        if (s.empty()) cout << -1 << ' ';
+        else cout << s.top() << ' ';
+        s.push(arr[i]);
     }
 
-    int before = INT_MAX;
-    while (!q.empty()) {
-        if (before < q.front()) {
-            cout << before << " ";
-        } else {
-            cout << -1 << " ";
-        }        
-        before = q.front();
-        q.pop();
-    }
-
-    return 0; 
+    return 0;
 }
