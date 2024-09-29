6958547015e3432c6df34835d5a49de4a4d96081
Temirlan <tzhanshin@mail.ru>
2024-09-29 20:19:32 +0500

    edited

--- lab4/A.cpp
+++ lab4/A.cpp
@@ -1,82 +1,92 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-template<typename T>
-class BST {
-public: 
-    BST() : size(0), root(nullptr) {}
-    ~BST() {
-        clear(root);
+struct Node {
+    int val;
+    Node *left;
+    Node *right;
+
+    Node (int x) {
+        this->val = x;
+        this->left = this->right = NULL;
+    }
+};
+
+struct Bst {
+    Node *root;
+
+    Bst () {
+        this->root = NULL;
     }
 
-    int get_size() {
-        return size;
-    }
-
-    void insert(T data) {
-        root = insert(root, data);
-    }
-
-    bool isPeak(const string& path) {
-        Node* current = find_node(root, path);
-        return (current != nullptr && current->left == nullptr && current->right == nullptr);
-    }
-
-private:
-    class Node {
-    public:
-        T data;
-        Node* left;
-        Node* right;
-
-        Node(T data = T(), Node* left = nullptr, Node* right = nullptr) {
-            this->data = data;
-            this->left = left;
-            this->right = right;
+    Node *add (Node *current, int x) {
+        if (current == NULL) {
+            current = new Node(x);
         }
-    };
-
-    int size;
-    Node* root;
-
-    Node* insert(Node* current, T data) {
-        if (current == nullptr) {
-            size++; return new Node(data); 
+        else if (current->val > x) {
+            if (current->left == NULL)
+                current->left = add(current->left, x);
+            else
+                add(current->left, x);
         }
-        if (data < current->data) current->left = insert(current->left, data);
-        else if (data > current->data) current->right = insert(current->right, data);
-        return current;
-    }
-
-    Node* find_node(Node* current, const string& path) {
-        for (char p : path) {
-            if (current == nullptr) return nullptr;
-            if (p == 'L') current = current->left;
-            else if (p == 'R') current = current->right;
+        else {
+            if (current->right == NULL)
+                current->right = add(current->right, x);
+            else
+                add(current->right, x);
         }
         return current;
     }
 
-    void clear(Node* current) {
-        if (current == nullptr) return;
-        clear(current->left);
-        clear(current->right);
-        delete current;
+    Node *search (Node *current, int x) {
+        if (current == NULL)
+            return NULL;
+        if (current->val == x)
+            return current;
+        else if (current->val > x)
+            return search(current->left, x);
+        else if (current->val < x)
+            return search(current->right, x);
+    }
+
+    void print (Node * current){
+        if(current != NULL){
+            print(current->left);
+            cout << current->val << " ";
+            print(current->right);
+        }
     }
 };
 
 int main() {
+    Bst *B = new Bst();
     int n, m;
     cin >> n >> m;
-    BST<int> bst;
-    while (n--) {
+    for (int i = 0; i < n; i++) {
         int x; cin >> x;
-        bst.insert(x);
+        if (B->root == NULL)
+            B->root = B->add(B->root, x);
+        else
+            B->add(B->root, x);
     }
-    while (m--) {
-        string path; cin >> path;
-        cout << (bst.isPeak(path) ? "YES" : "NO") << endl;
+    while(m--) {
+        string s; cin >> s;
+        Node *current = B->root;
+        for (int i = 0; i < s.length(); i++) {
+            if (current == NULL) {
+                break;
+            }
+            if (s[i] == 'L') {
+                current = current->left;
+            }
+            if (s[i] == 'R') {
+                current = current->right;
+            }
+        }
+        if (current != NULL)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
     }
-    return 0;
 }
